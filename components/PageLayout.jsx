95baa5cdd501924ad70a178c695d489dd68662d3
Marcos Aguado <marcosolda@outlook.es>
2022-08-20 19:06:49 -0300

    News App project using newsapi.org with for learn how to use Next.js

---
+++ components/PageLayout.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head"
+
+export default function PageLayout ({ children, title = 'NewsApp' }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="news-app - the best app to read news" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        newsapi
+      </header>
+      <main>
+        {children}
+      </main>
+      <style jsx>{`
+        header {
+          padding: 20px;
+        }
+      `}
+      </style>
+
+    </>
+)
+}
