15cd119b6a95638b07865e0a3e39024535516ded
Marcos Aguado <marcosolda@outlook.es>
2022-08-21 21:40:40 -0300

    added next/image component for image optimization and getStaticProps() for create static pages

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,16 @@ export default function Home({ articles }) {
 
         {articles.length > 0 && articles.map((article, index) => (
           <div key={index}>
-            <img src={article.urlToImage} alt={`Image for the article ${article}`} />
+            <Image 
+              src={article.urlToImage} 
+              alt={`Image for the article ${article}`}
+              width='450'
+              height='300'
+              layout='responsive'
+              quality={50}
+              priority={index < 2}
+              placeholder='empty'
+              />
             <h2>{article.title}</h2>
             <p>{article.description}</p>
           </div>
@@ -27,8 +36,31 @@ export default function Home({ articles }) {
   )
 }
 
-export async function getServerSideProps() {
-  const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-07-20&sortBy=publishedAt&apiKey=c2c374e104ca4b7d90e991491c85174e')
+
+  //  ESTE METODO SE EJECUTA EN EL SERVIDOR
+  // N request -> se ejecuta N veces
+  // para datos que necesitas que sean muy live
+  // o porque tiene demasiados datos dinamicos
+
+// export async function getServerSideProps() {
+//   const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-07-21&sortBy=publishedAt&apiKey=c2c374e104ca4b7d90e991491c85174e')
+
+//   const { articles } = await response.json()
+
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+  
+// }
+
+
+
+// Nos permite crear paginas estaticas.
+// N request -> se ejecuta 1 vez en build time (o para refrescar la pagina)
+export async function getStaticProps() {
+  const response = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c2c374e104ca4b7d90e991491c85174e')
 
   const { articles } = await response.json()
 
